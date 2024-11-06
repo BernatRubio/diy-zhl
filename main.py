68b323ca2207ee9af09fbf79f2ec20429c391608
BernatRubio <bernat.rubio.oliete@gmail.com>
2024-11-06 18:40:55 +0100

    wip

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from dive import Dive
+import copy
 
 dive_obj = Dive()
+tmp_obj = Dive()
 
-newdepth = 1.0
-# for i in range(5):
-#     newdepth += 0.5
-#     dive_obj.segment(newdepth, f"{i+1}:0")
-dive_obj.segment(2.5, f"{5}:0")
+bar = 5.0
 
+dive_obj.segment(bar, f"{5}:0")
+i = 0
+k = False
+tmp_obj = copy.deepcopy(dive_obj)
+while True:
+    i+=1
+    tmp_obj.segment(bar, f"{5+i}:0")
+    for ceiling in tmp_obj.ceilings:
+        if ceiling > 1:
+            k = True
+    if k:
+        break
+print(i)
+
+dive_obj.segment(5.0, f"{15}:0")
+dive_obj.segment(2.0, f"{20}:0")
 print(dive_obj.ceilings)
-
-#dive_obj.segment(dive_obj._P, f"{20}:0")
-
-#dive_obj.segment(2.0, f"{50}:0")
